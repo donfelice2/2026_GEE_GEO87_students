884b0a88ed2473bff82efe36f42e7baa3c58ee88
F. Bachofer <felix.bachofer@gmx.de>
2026-05-28 22:22:57 +0200

    Update M2_01_band_math.js

--- M2_01_band_math.js
+++ M2_01_band_math.js
@@ -1,5 +1,5 @@
 // =============================================================
-// 09 - NDVI the Hard Way: Manual Band Arithmetic
+// M2 01 -  NDVI the Hard Way: Manual Band Arithmetic
 // =============================================================
 // NDVI = (NIR - Red) / (NIR + Red)
 // Landsat 8: NIR = B5, Red = B4
@@ -8,6 +8,8 @@ var image = ee.Image('LANDSAT/LC08/C02/T1_TOA/LC08_195026_20190419');
 
 // TODO: Select the Red (B4) and NIR (B5) bands separately.
 
+var red = 
+var nir = 
 
 // TODO: Compute NDVI using .subtract(), .divide(), and .add().
 
@@ -16,3 +18,5 @@ var image = ee.Image('LANDSAT/LC08/C02/T1_TOA/LC08_195026_20190419');
 
 
 // QUESTION: What does an NDVI value close to 0 or negative represent?
+
+//Map.addLayer(ndvi_named, {min: 0, max: 1}, 'NDVI (manual)');
